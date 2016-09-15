e8f652d59dc3a3844feafb4f98b86a5e8e884ed7
Devin Cornell <devinj.cornell@gmail.com>
2016-09-15 15:28:56 -0700

    Submitting networkxstreamer.py

--- NetworkXStreamer.py
+++ NetworkXStreamer.py
@@ -18,52 +18,56 @@ import math
 class NetworkXStreamer():
     def __init__(self, baseGraph):
         # init streamer
-        self.stream = streamer.Streamer(streamer.GephiWS(hostname="localhost", port=8080, workspace="workspace1"), auto_commit=False)
+        self.stream = streamer.Streamer(streamer.GephiWS(hostname="localhost", port=8080, workspace="workspace1"), auto_commit=True)
 
         # add aditional nodes
-        numNodes = 10
-        self.nodes = []
+        nodeNames = baseGraph.nodes(data=False)
+        numNodes = len(nodeNames)
+        self.streamNodes = []
         for i in range(numNodes):
-            nodes.append(graph.Node(str(i)))
-            #self.nodes[i].property['weight'] = i/2.0
-            #self.nodes[i].property['size'] = 10.0
+            self.streamNodes.append(graph.Node(str(i)))
+            self.streamNodes[i].property['label'] = str(nodeNames[i])
+            #self.streamNodes[i].property['size'] = 10.0
+
             #TODO WAS UPDATING THIS BEFORE I GOT TIRED
         
-        self.stream.add_node(*self.nodes)
+        self.stream.add_node(*(self.streamNodes))
         
         # add initial edges
-        self.edges = []
-        for i in range(10):
-            for j in range(10):
-                property_value = (self.nodes[i].property['weight'] + self.nodes[j].property['weight'])/2.0
-                self.edges.append(graph.Edge(nodes[i],nodes[j], prop=property_value))
+        numEdges = len(baseGraph.edges(data=False))
+        self.streamEdges = []
+        for i in range(numNodes):
+            for j in range(numNodes):
+                #property_value = (self.streamNodes[i].property['weight'] + self.streamNodes[j].property['weight'])/2.0
+                #self.streamEdges.append(graph.Edge(nodes[i],nodes[j], prop=property_value))
+                self.streamEdges.append(graph.Edge(self.streamNodes[i],self.streamNodes[j],directed=False,label='',red=0.0,green=0.0,blue=0.0))
 
-        self.stream.add_edge(*edges)
+        self.stream.add_edge(*(self.streamEdges))
 
         # commit changes to visualization
-        self.stream.commit()
+        #self.stream.commit()
 
     def updateProperties(self, newGraph, nodeProperties, edgeProperties):
         '''Updates only the properties of the visualized graph with the newGraph properties.'''
         
-        # update nodes
+        # update node properties
         for p in nodeProperties:
-            nodes = nx.get_node_attributes(newGraph, p)
-            for k,v in nodes:
-                self.nodes[k] = v
+            nodeAttr = nx.get_node_attributes(newGraph, p)
+            for k,v in nodeAttr.items():
+                self.streamNodes[k].property[p] = v
         
-        self.stream.change_node(*self.nodes)
+        self.stream.change_node(*(self.streamNodes))
         
-        # update edges
+        # update edge properties
         for p in edgeProperties:
             edges = nx.get_edge_attributes(newGraph, p)
             for k,v in edges:
-                self.edges[k] = v
+                self.streamEdges[k].property[p] = v
         
-        self.stream.change_edge(*self.edges)
+        self.stream.change_edge(*(self.streamEdges))
 
         # commit changes to visualization
-        self.stream.commit()
+        #self.stream.commit()
 
     def updateEdgeConnections(self, newGraph):
         '''Updates edge connections between nodes.'''
@@ -78,14 +82,14 @@ if __name__ == '__main__':
     import time
     
     G = nx.Graph()
-    numNodes = 10
+    numNodes = 5
     
     # create nodes
+    nodeAttr = ('r','g','b','x','y')
     for i in range(numNodes):
         xPos = numNodes*10*math.sin(2*math.pi*i/numNodes)
         yPos = numNodes*10*math.cos(2*math.pi*i/numNodes)
         G.add_node(i, {'r':0.5,'g':0.1,'b':0.1, 'x':xPos, 'y':yPos})
-    nodeAttr = ('r','g','b','x','y')
 
     # create edges
     edgeAttr = ('r','g','b','weight')
@@ -93,23 +97,22 @@ if __name__ == '__main__':
         for j in range(numNodes):
             G.add_edge(i, j, {'r':0.1,'g':0.1,'b':0.5, 'weight':1, 'directed':False})
 
-    
     # start streaming
     nxs = NetworkXStreamer(G)
 
     # simulation loop
-    for i in range(50):
+    for i in range(1):
         # update node attr
         for attr in nodeAttr:
             av = nx.get_node_attributes(G, attr)
-            for k,v in av:
+            for k,v in av.items():
                 av[k] = v + 0.01
             nx.set_node_attributes(G, attr, av)
 
         # update edge attr
         for attr in edgeAttr:
             av = nx.get_edge_attributes(G, attr)
-            for k,v in av:
+            for k,v in av.items():
                 av[k] = v + 0.01
             nx.set_edge_attributes(G, attr, av)
 
@@ -117,6 +120,10 @@ if __name__ == '__main__':
         nxs.updateProperties(G,nodeAttr,edgeAttr)
 
         # pause
-        time.sleep(0.01)
-#
-#
+        print(i)
+        time.sleep(5.0)
+
+
+
+
+
